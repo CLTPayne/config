8328d336cdc6555fc1413ffedc5bcd0a88758c8d
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-08-21 14:37:30 +0100

    Add production config for deploying node
    * Add helmet and compression as dependencies for a node project to be
    deployed to heroku

---
+++ prod.js
@@ -0,0 +1,15 @@
+// Get node project production ready for deploying to heroku
+// Add the following file in the project root
+
+
+const helmet = require("helmet");
+const compression = require("compression");
+
+module.exports = function(app) {
+  app.use(helmet());
+  app.use(compression());
+};
+
+// Add the following in the app file:
+
+// require(''./prod')(app);
